e22ea25f994652f6007599220f5f2c070841deb7
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2024-11-10 18:10:35 +0200

    Challenge 2 -> The ternary operator checks if the number of cansLeft is greater than or equal to 5. If this condition is true, the expression evaluates to "We're stocked!"; otherwise, it evaluates to "Time to restock!". The result of this evaluation is stored in the show variable, and then the value is printed to the console.

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ console.log(`${show}`);
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
+show = (cansLeft >= 5) ? "We're stocked" : "Time to restock";
 console.log(`${show}`);
 
 
